301742f1097ec987a31e1bbce2ddc415566b17fa
GilbertoJRC20 <160493178+GilbertoJRC20@users.noreply.github.com>
2024-03-28 13:05:23 +0000

    final bdc 28/3/24
    Final da aula de Bases de Dados de 28/03/2024

--- FichaDeExercícios/SQLQuery04.sql
+++ FichaDeExercícios/SQLQuery04.sql
@@ -1,4 +1,6 @@
-﻿--DDL
+﻿use GestaoComercial;
+
+--DDL
 create table Cliente(
 	id int identity (1,1) primary key,
 	nome varchar (50) not null,
@@ -38,3 +40,70 @@ values (1,1, '2012-04-05', 1),
 select * from Cliente;
 select * from Produto;
 select * from Venda;
+
+update Venda
+set qtd = 10
+where prod = 1 
+		and cli = 2 
+		and data_venda =  '2012-04-05 00:00:00.000'
+
+
+create procedure alteraMorada (
+ @morada varchar (100), @cli int 
+)
+as
+	update Cliente
+	set morada = @morada
+	where id = @cli;
+go
+
+Exec alteraMorada
+@morada = 'Porto', @cli = 2;
+
+go
+
+--5.b
+create view clientesAtivos as
+select nome from Cliente
+where estado = 1
+go
+
+select * from clientesAtivos;
+go
+
+--5.e
+create procedure quantasVendas_cli (
+@cli int)
+as
+	select count(v.cli) as contagem
+	from venda v
+	where v.cli = @cli
+go
+Exec quantasVendas_cli @cli = 2
+go
+
+--5.f
+select max (preco)
+from produto
+go
+
+--5.f
+create view NomeProdMaisCaro as
+select descricao, preco
+from Produto
+where preco = (select max (preco) as Maxim
+					from produto)
+go
+
+select * from NomeProdMaisCaro
+go
+
+--5.h
+create view ClientesNuncaCompraram as
+select nome
+from Cliente
+where id not in (select cli from venda)
+go
+
+select * from ClientesNuncaCompraram
+go
